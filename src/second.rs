7c67616f6ec7e558226e0c36f6e9991807c8d291
Clover <blazelion3@gmail.com>
2018-06-24 13:25:00 -0400

    Added into_iter method.

--- src/second.rs
+++ src/second.rs
@@ -10,6 +10,8 @@ struct Node<T> {
     next: Link<T>,
 }
 
+pub struct IntoIter<T>(List<T>);
+
 impl<T> List<T> {
     pub fn new() -> Self {
         List { head: None }
@@ -42,6 +44,19 @@ impl<T> List<T> {
         self.head.as_mut().map(|node| {
             &mut node.elem
         })
+    }
+}
+
+impl<T> List<T> {
+    pub fn into_iter(self) -> IntoIter<T> {
+        IntoIter(self)
+    }
+}
+
+impl<T> Iterator for IntoIter<T> {
+    type Item = T;
+    fn next(&mut self) ->Option<Self::Item> {
+        self.0.pop()
     }
 }
 
@@ -98,5 +113,16 @@ mod test {
 
         assert_eq!(list.peek(), Some(&3));
         assert_eq!(list.peek_mut(), Some(&mut 3));
+    }
+
+    #[test]
+    fn into_iter() {
+        let mut list = List::new();
+        list.push(1);list.push(2);list.push(3);
+
+        let mut iter = list.into_iter();
+        assert_eq!(iter.next(), Some(3));
+        assert_eq!(iter.next(), Some(2));
+        assert_eq!(iter.next(), Some(1));
     }
 }
