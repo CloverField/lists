f0e030e86d8cacaa3a64487661a76e999266f894
Clover <blazelion3@gmail.com>
2018-06-24 08:33:14 -0400

    Fixed spacing.

--- src/first.rs
+++ src/first.rs
@@ -18,19 +18,21 @@ impl List {
     pub fn new() -> Self {
         List { head: Link::Empty }
     }
+
     pub fn push(&mut self, elem: i32) {
-        let new_node = Box::new(Node {
+        let _new_node = Box::new(Node {
             elem: elem,
             next: mem::replace(&mut self.head, Link::Empty),
         });
     }
+    
     pub fn pop(&mut self) -> Option<i32> {
         match self.head {
             Link::Empty => {
-
+                //TODO
             }
-            Link::More(ref node) => {
-                
+            Link::More(ref _node) => {
+                //TODO
             }
         };
         unimplemented!()
