420231d8a15d88663a97d17efaa3fc0fe0f5f113
Clover <blazelion3@gmail.com>
2018-06-24 07:46:47 -0400

    Oh hey this isnt so bad.

--- src/first.rs
+++ src/first.rs
@@ -1,3 +1,38 @@
-fn main() {
-    println!("Hello, world!");
+use std::mem;
+
+pub struct List {
+    head: Link,
+}
+
+enum Link {
+    Empty,
+    More(Box<Node>),
+}
+
+struct Node {
+    elem: i32,
+    next: Link,
+}
+
+impl List {
+    pub fn new() -> Self {
+        List { head: Link::Empty }
+    }
+    pub fn push(&mut self, elem: i32) {
+        let new_node = Box::new(Node {
+            elem: elem,
+            next: mem::replace(&mut self.head, Link::Empty),
+        });
+    }
+    pub fn pop(&mut self) -> Option<i32> {
+        match self.head {
+            Link::Empty => {
+
+            }
+            Link::More(ref node) => {
+                
+            }
+        };
+        unimplemented!()
+    }
 }
